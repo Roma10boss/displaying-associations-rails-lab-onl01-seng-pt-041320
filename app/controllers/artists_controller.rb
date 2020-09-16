f3b9745ea5e7ba08ba08bc37f04c4d98ea999cbe
Romario Gustave <romagustave10@gmail.com>
2020-09-16 06:08:36 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,15 +1,15 @@
 class ArtistsController < ApplicationController
   def index
-     @artists = Artist.all
-   end
+    @artists = Artist.all
+  end
 
-   def show
-     @artist = Artist.find(params[:id])
-   end
+  def show
+    @artist = Artist.find(params[:id])
+  end
 
-   def new
-     @artist = Artist.new
-   end
+  def new
+    @artist = Artist.new
+  end
 
   def create
     @artist = Artist.new(artist_params)
